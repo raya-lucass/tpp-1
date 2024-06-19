83e9410d069e00a4844869b2f354af23d059c50b
raya-lucass <lucas5raya@gmail.com>
2024-06-19 11:16:34 -0300

    ejercicio 5.py

---
+++ ejercicio 5.py
@@ -0,0 +1,9 @@
+datos_personales = {
+    'nombre': 'Matias',
+    'edad': 17,
+    'ciudad': 'Madrid'
+}
+print("Nombre:", datos_personales['nombre'])
+datos_personales['edad'] = 17
+datos_personales['género'] = 'Creemos que masculino'
+print("Datos personales actualizados:", datos_personales)
